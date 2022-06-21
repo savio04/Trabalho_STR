72afd73019713716a6a40998b305c487132f4225
savio04 <savio27.05.2001@gmail.com>
2022-06-20 22:54:31 -0300

    Fix: referencia.h and referencia.c

--- src/referenciaH.h
+++ src/referenciaH.h
@@ -1,22 +1,7 @@
-/* Monitor sensor, no arquivo sensor.c */ 
-#include <math.h>
-#include <pthread.h>
+#ifndef REFERENCIAT_H
+#define REFERENCIAT_H
 
-static pthread_mutex_t exclusao_mutua = PTHREAD_MUTEX_INITIALIZER; 
-static double ref_entrada = 0; 
-static double ref_entrada_nivel = 0;
-static double limite_atual = HUGE_VAL;
+void ref_putH(double ref);
+double ref_getH(void);
 
-void ref_putH(double ref) {
-	pthread_mutex_lock( &exclusao_mutua); 
-	ref_entrada_nivel = ref; 
-	pthread_mutex_unlock( &exclusao_mutua); 
-}
-
-double ref_getH(void) {
-	double aux; 
-	pthread_mutex_lock( &exclusao_mutua); 
-	aux = ref_entrada_nivel; 
-	pthread_mutex_unlock( &exclusao_mutua); 
-	return aux;
-}
+#endif
